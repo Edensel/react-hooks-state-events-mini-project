f994ba895c07b0b08c4c332fcb93fd6112a64cfa
DenselEsekon <densel.esekon@moringaschool.com>
2023-12-03 14:06:46 +0300

    Completed react mini project

--- src/components/NewTaskForm.js
+++ src/components/NewTaskForm.js
@@ -1,16 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 
-function NewTaskForm() {
+function NewTaskForm({ categories, onTaskFormSubmit }) {
+  const [formData, setFormData] = useState({
+    text: "",
+    category: categories[0], // Set a default category if available
+  });
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const submitForm = (e) => {
+    e.preventDefault();
+    onTaskFormSubmit(formData);
+    // Reset form data after submission if needed
+    setFormData({ text: "", category: categories[0] });
+  };
+
   return (
-    <form className="new-task-form">
+    <form onSubmit={submitForm} className="new-task-form">
       <label>
         Details
-        <input type="text" name="text" />
+        <input
+          type="text"
+          required
+          name="text"
+          value={formData.text}
+          onChange={handleInputChange}
+        />
       </label>
       <label>
         Category
-        <select name="category">
-          {/* render <option> elements for each category here */}
+        <select name="category" value={formData.category} onChange={handleInputChange}>
+          {categories.map((category) => (
+            <option key={category} value={category}>
+              {category}
+            </option>
+          ))}
         </select>
       </label>
       <input type="submit" value="Add task" />
